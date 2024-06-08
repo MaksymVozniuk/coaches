f7b75f876496825698bba7a419ac30e804621301
Maksym <mmaakkss602@gmail.com>
2024-06-08 17:19:46 +0300

    Add filter and list of coaches sections with controls

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<script></script>
 
-<template></template>
+<template>
+  <section>Filter</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul>
+      List of Coaches
+    </ul>
+  </section>
+</template>
 
 <style scoped></style>
