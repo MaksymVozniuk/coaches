eef130f7866da3af13d26ff11afc5ca94451eea0
Maksym <mmaakkss602@gmail.com>
2024-06-08 17:54:56 +0300

    Add coach filtering and display functionality with Vuex

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,15 @@
-<script></script>
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
 
 <template>
   <section>Filter</section>
@@ -7,9 +18,12 @@
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>
-      List of Coaches
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }} {{ coach.lastName }}
+      </li>
     </ul>
+    <h3 v-else>No Coaches Found</h3>
   </section>
 </template>
 
