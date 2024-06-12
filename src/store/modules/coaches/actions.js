087a8f16653df730fb3eefa0ea42c9d9f0197da0
Maksym <mmaakkss602@gmail.com>
2024-06-12 21:57:18 +0300

    Add 'addCoach' function for adding coaches

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  addCoach(context, data) {
+    const coachData = {
+      id: `c${context.state.coaches.length + 1}`,
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
+    context.commit('addCoach', coachData);
+  },
+};
