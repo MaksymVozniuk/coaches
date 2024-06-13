8e3f2bf5c1be3cf1ba13c7a9c9dc96facd298e23
Maksym <mmaakkss602@gmail.com>
2024-06-13 14:24:01 +0300

    Reorganize Vue Router routes for better readability

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 
@@ -15,7 +16,9 @@ const router = createRouter({
       path: '/coaches/:id',
       component: CoachDetail,
       props: true,
-      children: [{ path: 'contact', component: ContactCoach }],
+      children: [
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+      ],
     },
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestsReceived },
