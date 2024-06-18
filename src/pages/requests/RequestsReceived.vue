30b5720b934b44d3e6459876ae3ec212b80bed11
Maksym <mmaakkss602@gmail.com>
2024-06-19 02:57:09 +0300

    feat(components): add RequestsReceived component

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,5 +1,87 @@
-<script></script>
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+import BaseDialog from '@/components/ui/BaseDialog.vue';
+import BaseSpinner from '@/components/ui/BaseSpinner.vue';
 
-<template>Requests</template>
+export default {
+  components: {
+    BaseSpinner,
+    BaseDialog,
+    RequestItem,
+  },
+  data() {
+    return {
+      error: null,
+      isLoading: false,
+    };
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something failed!';
+      }
+      this.isLoading = false;
+    },
+  },
+  created() {
+    this.loadRequests();
+  },
+};
+</script>
 
-<style scoped></style>
+<template>
+  <div>
+    <base-dialog
+      :show="!!error"
+      title="An error occurred"
+      @close="error = null"
+    >
+      <p>{{ error }}</p>
+    </base-dialog>
+    <section>
+      <base-card>
+        <header>
+          <h2>Requests Received</h2>
+        </header>
+        <base-spinner v-if="isLoading"></base-spinner>
+        <ul v-else-if="hasRequests && !isLoading">
+          <request-item
+            v-for="req in receivedRequests"
+            :key="req.id"
+            :email="req.userEmail"
+            :message="req.message"
+          ></request-item>
+        </ul>
+        <h3 v-else>You haven't received any requests yet!</h3>
+      </base-card>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
