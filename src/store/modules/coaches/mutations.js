9c20be1b257ac21357edd58f2bced69cf7b66c7f
Maksym <mmaakkss602@gmail.com>
2024-06-19 02:53:47 +0300

    feat(store): add registerCoach mutation and update mutations.js
    - Added the registerCoach mutation to handle registering a new coach in the state.
    - Updated the setCoaches mutation to set the coaches array in the state.
    - Added the setFetchTimestamp mutation to update the lastFetch timestamp in the state.

--- src/store/modules/coaches/mutations.js
+++ src/store/modules/coaches/mutations.js
@@ -1,5 +1,11 @@
 export default {
-  addCoach(state, payload) {
+  registerCoach(state, payload) {
     state.coaches.push(payload);
+  },
+  setCoaches(state, payload) {
+    state.coaches = payload;
+  },
+  setFetchTimestamp(state) {
+    state.lastFetch = new Date().getTime();
   },
 };
